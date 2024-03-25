4ae876f9bbb295da79c86509fc89905fb04287f0
blessedmadukoma <blessedmadukoma@gmail.com>
2024-03-25 23:22:10 +0100

    add tests for get fixtures, get fixture and remove fixture

--- api/fixtures_e2e_test.go
+++ api/fixtures_e2e_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -15,20 +16,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCreateFixturesE2E(t *testing.T) {
+func (srv *Server) createTestFixtureData(t *testing.T, ts *httptest.Server) *http.Response {
 	ctx := context.Background()
-
-	mongoclient, collections := db.ConnectMongoDB(ctx, config)
-
-	defer mongoclient.Disconnect(ctx)
-
-	assert.NotNil(t, collections)
-
-	srv, err := NewServer(config, collections, redisclient)
-	assert.NoError(t, err)
-
-	ts := httptest.NewServer(srv.router)
-	defer ts.Close()
 
 	token := srv.obtainAdminAuthToken(t, ts)
 
@@ -42,7 +31,7 @@ func TestCreateFixturesE2E(t *testing.T) {
 		UpdatedAt: time.Now(),
 	}
 
-	_, err = srv.collections["teams"].InsertOne(ctx, homeParams)
+	_, err := srv.collections["teams"].InsertOne(ctx, homeParams)
 	assert.NoError(t, err)
 
 	awayParams := models.CreateTeamsParams{
@@ -73,6 +62,26 @@ func TestCreateFixturesE2E(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	return res
+}
+
+func TestCreateFixturesE2E(t *testing.T) {
+	ctx := context.Background()
+
+	mongoclient, collections := db.ConnectMongoDB(ctx, config)
+
+	defer mongoclient.Disconnect(ctx)
+
+	assert.NotNil(t, collections)
+
+	srv, err := NewServer(config, collections, redisclient)
+	assert.NoError(t, err)
+
+	ts := httptest.NewServer(srv.router)
+	defer ts.Close()
+
+	res := srv.createTestFixtureData(t, ts)
 	defer res.Body.Close()
 
 	assert.Equal(t, http.StatusCreated, res.StatusCode)
@@ -102,6 +111,163 @@ func TestGetFixtures(t *testing.T) {
 	token := srv.obtainFanAuthToken(t, ts)
 
 	req, err := http.NewRequest("GET", ts.URL+"/api/fixtures", nil)
+	req.Header.Set("Authorization", "Bearer "+token)
+	req.Header.Set("Content-Type", "application/json")
+	assert.NoError(t, err)
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	assert.Equal(t, http.StatusOK, res.StatusCode)
+
+	// Decode the response body
+	var responseBody map[string]interface{}
+	err = json.NewDecoder(res.Body).Decode(&responseBody)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestGetFixtureByLink(t *testing.T) {
+	ctx := context.Background()
+
+	mongoclient, collections := db.ConnectMongoDB(ctx, config)
+
+	defer mongoclient.Disconnect(ctx)
+
+	assert.NotNil(t, collections)
+
+	srv, err := NewServer(config, collections, redisclient)
+	assert.NoError(t, err)
+
+	ts := httptest.NewServer(srv.router)
+	defer ts.Close()
+
+	token := srv.obtainFanAuthToken(t, ts)
+
+	response := srv.createTestFixtureData(t, ts)
+	defer response.Body.Close()
+
+	assert.Equal(t, http.StatusCreated, response.StatusCode)
+
+	var fixtureResponseBody map[string]interface{}
+	err = json.NewDecoder(response.Body).Decode(&fixtureResponseBody)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fixtureLink := fixtureResponseBody["data"].(map[string]interface{})["fixture"].(map[string]interface{})["link"].(string)
+
+	req, err := http.NewRequest("GET", ts.URL+"/api/fixtures/link/"+fixtureLink, nil)
+	req.Header.Set("Authorization", "Bearer "+token)
+	req.Header.Set("Content-Type", "application/json")
+	assert.NoError(t, err)
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	assert.Equal(t, http.StatusOK, res.StatusCode)
+
+	// Decode the response body
+	var responseBody map[string]interface{}
+	err = json.NewDecoder(res.Body).Decode(&responseBody)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestGetFixtureByID(t *testing.T) {
+	ctx := context.Background()
+
+	mongoclient, collections := db.ConnectMongoDB(ctx, config)
+
+	defer mongoclient.Disconnect(ctx)
+
+	assert.NotNil(t, collections)
+
+	srv, err := NewServer(config, collections, redisclient)
+	assert.NoError(t, err)
+
+	ts := httptest.NewServer(srv.router)
+	defer ts.Close()
+
+	token := srv.obtainFanAuthToken(t, ts)
+
+	response := srv.createTestFixtureData(t, ts)
+	defer response.Body.Close()
+
+	assert.Equal(t, http.StatusCreated, response.StatusCode)
+
+	var fixtureResponseBody map[string]interface{}
+	err = json.NewDecoder(response.Body).Decode(&fixtureResponseBody)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	log.Println("fixtureResponseBody:", fixtureResponseBody)
+
+	fixtureID := fixtureResponseBody["data"].(map[string]interface{})["fixture"].(map[string]interface{})["id"].(string)
+
+	log.Println("fixtureID:", fixtureID)
+
+	req, err := http.NewRequest("GET", ts.URL+"/api/fixtures/"+fixtureID, nil)
+	req.Header.Set("Authorization", "Bearer "+token)
+	req.Header.Set("Content-Type", "application/json")
+	assert.NoError(t, err)
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	assert.Equal(t, http.StatusOK, res.StatusCode)
+
+	// Decode the response body
+	var responseBody map[string]interface{}
+	err = json.NewDecoder(res.Body).Decode(&responseBody)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestRemoveFixture(t *testing.T) {
+	ctx := context.Background()
+
+	mongoclient, collections := db.ConnectMongoDB(ctx, config)
+
+	defer mongoclient.Disconnect(ctx)
+
+	assert.NotNil(t, collections)
+
+	srv, err := NewServer(config, collections, redisclient)
+	assert.NoError(t, err)
+
+	ts := httptest.NewServer(srv.router)
+	defer ts.Close()
+
+	token := srv.obtainAdminAuthToken(t, ts)
+
+	response := srv.createTestFixtureData(t, ts)
+	defer response.Body.Close()
+
+	assert.Equal(t, http.StatusCreated, response.StatusCode)
+
+	var fixtureResponseBody map[string]interface{}
+	err = json.NewDecoder(response.Body).Decode(&fixtureResponseBody)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fixtureID := fixtureResponseBody["data"].(map[string]interface{})["fixture"].(map[string]interface{})["id"].(string)
+
+	req, err := http.NewRequest("DELETE", ts.URL+"/api/fixtures/"+fixtureID, nil)
 	req.Header.Set("Authorization", "Bearer "+token)
 	req.Header.Set("Content-Type", "application/json")
 	assert.NoError(t, err)
